ea0fb77cba181d53be4fb2ded558c6953eb4ef56
ZG5991 <92684029+ZG5991@users.noreply.github.com>
2021-10-17 13:23:22 -0500

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,23 @@
+# Write your code here :-)
+"this is a simple code."
+
+
+print("hello there!")
+print("state your name, stranger.")  # ask for their name
+
+myname = input()
+
+print("good to meet you, " + myname)
+print("what do you like to do for fun?")
+
+funactivity = input()
+
+print("wow, " + funactivity + ", that sounds neat!!")
+
+input()
+
+print("I see, so, in addition to " + funactivity + ", what's favorite food?")
+
+favfood = input()
+
+print("ah, " + favfood + "! holy nutballs, that sounds good.")
